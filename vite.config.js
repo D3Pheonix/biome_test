775e723ed8a624548884e0b5dff67fb5452254f1
anonym <bvrus93@gmail.com>
2025-06-23 12:08:00 +0300

    vite bug

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 // some comment
-// some comment
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [react(), tsconfigPaths(), svgr()],
 	form,
 });
+
